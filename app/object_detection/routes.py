1a9469bdd5bc46e9f9507f655acc0ab02769ff35
David Grijalva <dgrijalva@indeed.com>
2022-02-04 15:01:07 -0600

    logic changes

--- app/object_detection/routes.py
+++ app/object_detection/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, Response
-from app.object_detection.video import gen_frames
+from app.object_detection.video_client import gen_frames_client
 
 object_detection_blueprint = Blueprint(
     "object_detection_blueprint", __name__, template_folder="templates")
@@ -12,4 +12,4 @@ def index():
 
 @object_detection_blueprint.route("/camera", methods=["GET", "POST"])
 def activate_camera():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames_client(), mimetype='multipart/x-mixed-replace; boundary=frame')
