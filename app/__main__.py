3d9398c40ec63a010ab008122287854a5449b2c8
David Grijalva <dgrijalva@indeed.com>
2022-02-09 18:07:01 -0600

    structure changes

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit
-from io import StringIO
-import io
-import base64
-from PIL import Image
-import cv2
-import numpy as np
-import imutils
+from flask_socketio import SocketIO
 
 from app.object_detection.video import get_video_preds
 
@@ -24,34 +17,6 @@ def create_app():
 
 
 daemon_app = create_app()
-
-
-@socketio.on('image')
-def image(data_image):
-    sbuf = StringIO()
-    sbuf.write(data_image)
-    print(sbuf)
-
-    # decode and convert into image
-    b = io.BytesIO(base64.b64decode(data_image))
-    pimg = Image.open(b)
-
-    ## converting RGB to BGR, as opencv standards
-    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
-
-    # get predictions
-    frame, preds = get_video_preds(frame)
-    print(preds)
-    imgencode = cv2.imencode('.jpg', frame)[1]
-
-
-    # base64 encode
-    stringData = base64.b64encode(imgencode).decode('utf-8')
-    b64_src = 'data:image/jpg;base64,'
-    stringData = b64_src + stringData
-
-    # emit the frame back
-    emit('response_back', stringData)
 
 
 if __name__ == "__main__":
---
+++ app/object_detection/events.py
@@ -0,0 +1,36 @@
+from flask_socketio import  emit
+from io import StringIO
+import io
+import base64
+from PIL import Image
+import cv2
+import numpy as np
+
+
+from ..__main__ import socketio
+
+
+@socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+    print(sbuf)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    ## converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # get predictions
+    frame, preds = get_video_preds(frame)
+    print(preds)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+    # emit the frame back
+    emit('response_back', stringData)
